90c36f3df1c0a53cc8b07b13b6a45fe19da2d273
sistemas <sigpeauxsistemas@gmail.com>
2022-08-29 18:21:45 -0500

    corriendo mensaje

--- resources/views/email-template.blade.php
+++ resources/views/email-template.blade.php
@@ -2,7 +2,7 @@
 
     <h3>El departamento de seguridad industrial de DRUMMONTLTD</h3>
 
-    <p>Le informa que se ha generado el informe de la inspección realizada el dia <b> {{ $fecha }} </b> en su área <b> {{$area}} </b>
+    <p>Le informa que se ha generado el informe de la inspección realizada el dia <b> {{ $fecha }} </b> en el área de <b> {{$area}} </b>
         con el acompañamiento del supervisor de seguridad industrial <b> "{{$inspector}}" </b> y el delegado del área <b> "{{$delegado}}" </b> . </p> 
     <p>Link de descargar aqui => {{ $link }} </p>
     <br><br>
